ad7b5869f94a59d646534545a586bf319faa4571
Azizkhuja <azizkhujagulom@gmail.com>
2023-03-14 16:17:58 +0500

    Azizkhuja changed

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log('hello world');
+const btn = document.querySelector('.btn')
+const counter = document.querySelector('.counter')
+
+let a = 0
+btn.addEventListener('click', () => {
+    if(a < 11){
+        counter.textContent = `${a++}`;
+    }
+    if(a == 10 ){
+        alert('tugadi')
+    }
+});
